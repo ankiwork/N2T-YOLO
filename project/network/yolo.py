28accbf8361da15cb08dd5842f63dbd4a48ec072
Antonov Kirill Alekseevich <115501085+ankiwork@users.noreply.github.com>
2024-12-13 21:33:29 +0300

    Upload code v1.0.2

--- project/network/yolo.py
+++ project/network/yolo.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-from project.configuration.yolo.version.processing_version import load_yolo_version
+from project.configuration.yolo.data_processing import load_data
 
 
 def start_training():
@@ -14,22 +14,30 @@ def train_yolo_model():
     Возвращает:
     - results: Результаты обучения.
     """
+    # Определение вычислительных модулей
+    device = load_data("project/configuration/yolo/data/selected_yolo_device.txt")
+
+    # Определение вычислительных модулей
+    epochs = load_data("project/configuration/yolo/data/selected_yolo_epochs.txt")
+
+    # Определение вычислительных модулей
+    image = load_data("project/configuration/yolo/data/selected_yolo_image.txt")
+
     # Загрузка предобученной модели
-    model_name = load_yolo_version()
-    model = YOLO(model_name)
+    model = load_data("project/configuration/yolo/data/selected_yolo_version.txt")
+    model = YOLO(model)
 
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
+    # Определение расположения файла data.yaml
     data_yaml_path = "datasets/data.yaml"
 
-    # Начало обучения на пользовательском наборе данных
+    # Начало обучения на пользовательском наборе данных с сохранением промежуточных результатов
     results = model.train(
-        batch=16,
-        imgsz=640,
-        epochs=10,
+        batch=8,
         name="test",
-        data=data_yaml_path,
-        device='cpu'
+        device=device,
+        imgsz=int(image),
+        epochs=int(epochs),
+        data=data_yaml_path
     )
 
     return results
