29f88b08073986231202d2ee5170215fd70a0406
Svin777 <ar.nazarenko@mail.ru>
2025-02-19 09:55:15 +0300

    Added settings categories

--- project/application/layers/settings.py
+++ project/application/layers/settings.py
@@ -6,102 +6,69 @@ from project.application.backend.cascade_control import reset_settings
 
 
 def create_settings_layer():
-    """
-    Создает вкладку "settings".
-
-    Параметры:
-    None
-
-    Возвращает:
-    settings_tab: Сформированная вкладка.
-    """
     settings_tab = Tab(text="Settings")
 
-    settings_container = Row(
-        alignment=MainAxisAlignment.CENTER,
-        spacing=50
-    )
-
-    yolo_device = ["cpu", "gpu"]
-    yolo_epochs = [100, 200, 300]
-    yolo_image = [320, 640, 1280]
-    yolo_versions = ["yolo11m.pt", "yolo11m-seg.pt", "yolo11l.pt", "yolo11l-seg.pt", "yolo11x.pt", "yolo11x-seg.pt"]
-    yolo_bool = ["True", "False"]
-
-    dropdowns = [
-        ("Тип графического устройства", yolo_device, load_data("Тип графического устройства")),
-        ("Количество эпох", yolo_epochs, load_data("Количество эпох")),
-        ("Размер изображения", yolo_image, load_data("Размер изображения")),
-        ("Версия YOLO", yolo_versions, load_data("Версия YOLO")),
-        ("Контрольные точки обучения", yolo_bool, load_data("Контрольные точки обучения")),
-        ("Кэширование изображений", yolo_bool, load_data("Кэширование изображений")),
-        ("Перезапись существующего названия", yolo_bool, load_data("Перезапись существующего названия")),
-        ("Начинать ли обучение с предварительно обученной модели", yolo_bool,
-         load_data("Начинать ли обучение с предварительно обученной модели")),
-        ("Детерминированные алгоритмы", yolo_bool, load_data("Детерминированные алгоритмы")),
-        ("Мега класс", yolo_bool, load_data("Мега класс")),
-        ("Прямоугольное обучение", yolo_bool, load_data("Прямоугольное обучение"))
+    categories = [
+        ("Общие параметры", [
+            ("Тип графического устройства", ["cpu", "gpu"]),
+            ("Версия YOLO",
+             ["yolo11m.pt", "yolo11m-seg.pt", "yolo11l.pt", "yolo11l-seg.pt", "yolo11x.pt", "yolo11x-seg.pt"]),
+            ("Кэширование изображений", ["True", "False"])
+        ]),
+        ("Параметры обучения", [
+            ("Количество эпох", [100, 200, 300]),
+            ("Контрольные точки обучения", ["True", "False"]),
+            ("Детерминированные алгоритмы", ["True", "False"]),
+            ("Прямоугольное обучение", ["True", "False"])
+        ]),
+        ("Параметры ввода/вывода", [
+            ("Разовое количество фотографий", r"^\d+$"),
+            ("Количество потоков-работников", r"^\d+$"),
+            ("Имя директории проекта", r"^[a-zA-Z0-9_-]+$")
+        ])
     ]
 
-    input_fields = [
-        ("Разовое количество фотографий", load_data("Разовое количество фотографий"), r"^\d+$"),
-        ("Количество потоков-работников", load_data("Количество потоков-работников"), r"^\d+$"),
-        ("Ожидающие эпохи", load_data("Ожидающие эпохи"), r"^\d+$"),
-        ("Скорость обновления весов", load_data("Скорость обновления весов"), r"^-?\d+(?:\.\d+)?$"),
-        ("Частота сохранения контрольных точек", load_data("Частота сохранения контрольных точек"),
-         r"^-?\d+(?:\.\d+)?$"),
-        ("Случайное зерно для обучения", load_data("Случайное зерно для обучения"), r"^-?\d+$"),
-        ("Имя директории проекта", load_data("Имя директории проекта"), r"^[a-zA-Z0-9_-]+$"),
-        ("Имя тренировочного прогона", load_data("Имя тренировочного прогона"), r"^[a-zA-Z0-9_-]+$"),
-        ("Список классов", load_data("Список классов"), r"^[a-zA-Z0-9, ]+$")
-    ]
+    settings_container = Column(spacing=10)
 
-    dropdown_column = Column(spacing=10,
-                             controls=[],
-                             alignment=MainAxisAlignment.CENTER
-                             )
-    textfield_column = Column(spacing=10,
-                              controls=[],
-                              alignment=MainAxisAlignment.CENTER
-                              )
+    for category, options in categories:
+        controls = []
+        for label, values in options:
+            default = load_data(label)
 
-    for label, options, default in dropdowns:
-        default = default if default in options else "параметр не найден"
-        setting = Dropdown(
-            label=label,
-            options=[dropdown.Option(str(option)) for option in options],
-            value=str(default),
-            on_change=lambda e: save_data(e.control.value, e.control.label),
-            width=250,
-        )
-        dropdown_column.controls.append(setting)
+            if isinstance(values, list):  # Dropdown
+                default = default if default in values else "параметр не найден"
+                dropdown_setting = Dropdown(
+                    label=label,
+                    options=[dropdown.Option(str(option)) for option in values],
+                    value=str(default),
+                    on_change=lambda e: save_data(e.dropdown_setting.value, e.dropdown_setting.label),
+                    width=250,
+                )
+                controls.append(dropdown_setting)
+            else:  # TextField
+                pattern = values
+                textfield_setting = TextField(
+                    label=label,
+                    value=str(default) if default is not None else "",
+                    on_change=lambda e: save_data(e.textfield_setting.value, e.textfield_setting.label)
+                    if re.match(pattern, e.textfield_setting.value) else None,
+                    width=250,
+                )
+                controls.append(textfield_setting)
 
-    for label, default, pattern in input_fields:
-
-        text_field = TextField(
-            label=label,
-            value=str(default) if default is not None else "",
-            on_change=lambda e: save_data(e.control.value, e.control.label) if re.match(pattern,
-                                                                                        e.control.value) else None,
-            width=250,
-        )
-        textfield_column.controls.append(text_field)
+        settings_container.controls.append(ExpansionTile(title=Text(category), controls=controls))
 
     button_row = Row(
         alignment=MainAxisAlignment.CENTER,
-        spacing=5,
         controls=[
-            Container(width=10),
             ElevatedButton(
                 "Сбросить настройки",
-                on_click=lambda e: reset_settings(textfield_column.controls, dropdown_column.controls, settings_tab),
-                width=150
+                on_click=lambda e: reset_settings(settings_container.controls, settings_tab),
+                width=200
             )
         ]
     )
 
-    settings_container.controls.append(dropdown_column)
-    settings_container.controls.append(textfield_column)
     settings_container.controls.append(button_row)
     settings_tab.content = settings_container
 
