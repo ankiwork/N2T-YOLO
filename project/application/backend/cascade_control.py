d242d55b3b710ddc10c4ab8384dd859079f750b4
Svin777 <ar.nazarenko@mail.ru>
2025-02-20 12:20:23 +0300

    Added appdata settings-file check

--- project/application/backend/cascade_control.py
+++ project/application/backend/cascade_control.py
@@ -20,10 +20,10 @@ def check_file_settings():
         os.makedirs(data_folder)
 
     if not os.path.exists(settings_file):
-        with open(settings_file, "w") as f:
+        with open(settings_file, "w", encoding="utf-8") as f:
             json.dump(default_settings, f, indent=4)
 
-    with open(settings_file, "r") as f:
+    with open(settings_file, "r", encoding="utf-8") as f:
         settings = json.load(f)
 
     return settings.get("launch_denial")
@@ -39,7 +39,7 @@ def update_launch_settings():
     Возвращает:
     None
     """
-    with open(settings_file, "r") as f:
+    with open(settings_file, "r", encoding="utf-8") as f:
         settings = json.load(f)
         if settings["launch_denial"] == 1:
             settings["launch_denial"] = 0
@@ -83,7 +83,7 @@ def reset_settings(controls, settings):
                 label = LABEL_TO_KEY.get(control.label)
                 control.value = type(control.value)(default_settings.get(label))
 
-    with open(settings_file, "w") as f:
+    with open(settings_file, "w", encoding="utf-8") as f:
         default_settings["launch_denial"] = 1
         json.dump(default_settings, f, indent=4)
 
--- project/network/yolo.py
+++ project/network/yolo.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import json
 import logging
 import threading
 from ultralytics import YOLO
@@ -30,16 +32,12 @@ def train_yolo_model(log_widget):
 
     logging.basicConfig(stream=sys.stderr, level=logging.INFO, force=True)  # Перенаправляем logging
 
-    # Определение вычислительных модулей
     device = 0 if (load_data("Тип графического устройства")) == 0 else 'cpu'
 
-    # Определение количества эпох
     epochs = load_data("Количество эпох")
 
-    # Определение размера изображения
     image = load_data("Размер изображения")
 
-    # Определение расположения файла data.yaml
     data_yaml_path = "datasets/data.yaml"
 
     name = load_data("Имя тренировочного прогона")
@@ -52,23 +50,33 @@ def train_yolo_model(log_widget):
 
     lr0 = load_data("Скорость обновления весов")
 
-    # Загрузка предобученной модели
     model = load_data("Версия YOLO")
 
     model = YOLO(model)
-    # Начало обучения на пользовательском наборе данных с сохранением промежуточных результатов
-    results = model.train(
-        batch=int(batch),              # Количество изображений, обрабатываемых за один раз
-        workers=int(workers),            # Количество потоков-работников для загрузки данных
-        patience=int(patience),           # Эпохи ожидания без заметного улучшения
-        lr0=float(lr0),           # Определяет, насколько быстро модель будет обновлять свои веса
-        name=name,          # Имя для текущего запуска обучения
-        device=device,        # Устройство для выполнения обучения
-        optimizer='SGD',      # Оптимизатор, который следует использовать для обучения
-        imgsz=int(image),     # Размер входного изображения
-        epochs=int(epochs),   # Количество эпох обучения
-        data=data_yaml_path,  # Путь к файлу конфигурации набора данных
-    )
+
+    path_to_ultralytics = (os.environ['APPDATA'] + "\\Ultralytics\\settings.json")
+
+    with open(path_to_ultralytics, "r", encoding="utf-8") as f:
+        dataset_path = json.load(f)
+        if dataset_path["datasets_dir"] != "C:\\Users\\Artyom\\Desktop\\N2T-YOLO\\datasets":
+            with open(path_to_ultralytics, "w", encoding="utf-8") as f:
+                dataset_path["datasets_dir"] = "C:\\Users\\Artyom\\Desktop\\N2T-YOLO\\datasets"
+                json.dump(dataset_path, f, indent=0)
+    try:
+        results = model.train(
+            batch=int(batch),              # Количество изображений, обрабатываемых за один раз
+            workers=int(workers),            # Количество потоков-работников для загрузки данных
+            patience=int(patience),           # Эпохи ожидания без заметного улучшения
+            lr0=float(lr0),           # Определяет, насколько быстро модель будет обновлять свои веса
+            name=name,          # Имя для текущего запуска обучения
+            device=device,        # Устройство для выполнения обучения
+            optimizer='SGD',      # Оптимизатор, который следует использовать для обучения
+            imgsz=int(image),     # Размер входного изображения
+            epochs=int(epochs),   # Количество эпох обучения
+            data=data_yaml_path,  # Путь к файлу конфигурации набора данных
+        )
+    except RuntimeError:
+        pass
 
     update_launch_settings()
     print("Обучение завершено. Результаты:", results)
