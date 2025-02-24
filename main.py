8089fdcef2454b3cd66236c924d36885eb7ca620
Svin777 <ar.nazarenko@mail.ru>
2025-02-24 10:29:54 +0300

    Major fix of launch system

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import atexit
 from flet import app
 
 from project.application.build import building_the_application
-from project.application.backend.cascade_control import check_file_settings, update_launch_settings, cleanup
+from project.application.backend.cascade_control import check_file_settings
 
-atexit.register(cleanup)
+check_file_settings()
 
-if check_file_settings() == 0:
-    update_launch_settings()
-    app(target=building_the_application)
+app(target=building_the_application)
