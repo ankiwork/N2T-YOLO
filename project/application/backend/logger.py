4e51cacc8e607b75cc0d725adbbc8575207f7aee
Svin777 <ar.nazarenko@mail.ru>
2025-02-26 16:51:40 +0300

    Working on logger

--- project/application/backend/logger.py
+++ project/application/backend/logger.py
@@ -73,10 +73,13 @@ class LogRedirector(io.TextIOBase):
             formatted_message = f"{parts.get('epoch')} - {parts.get('percent')}|{progress_bar}| {parts.get('progress')}"
 
             with self.lock:
-                self.text_widget.value = ""
+                #self.text_widget.value = ""
                 self.text_widget.value += "Epoch                     Progress               Remaining time" + "\n"
                 self.text_widget.value += formatted_message + "\n"
                 self.text_widget.update()
+        else:
+            self.text_widget.value += message
+            self.text_widget.update()
 
     def flush(self):
         pass
--- project/network/yolo.py
+++ project/network/yolo.py
@@ -1,11 +1,42 @@
 import sys
 import logging
 import threading
-from ultralytics import YOLO
+import json
+import os
 
-from project.network.checks import retry
+from ultralytics.utils import LOGGER
 from project.application.backend.logger import LogRedirector
 from project.configuration.yolo.data_processing import load_data
+
+
+def get_datasets_path():
+    """
+    Ищет путь до папки с файлами для обучения YOLO
+
+    Возвращает:
+    - datasets_path (str): путь до папки с файлами
+    """
+    # Получаем абсолютный путь к текущему файлу (checks.py)
+    current_file = os.path.abspath(__file__)
+
+    # Поднимаемся на три уровня вверх, чтобы попасть в корень проекта
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file)))
+
+    # Формируем путь к папке datasets
+    datasets_path = os.path.join(project_root, "datasets")
+
+    return datasets_path
+
+
+path_to_ultralytics = (os.environ['APPDATA'] + "\\Ultralytics\\settings.json")
+with open(path_to_ultralytics, "r", encoding="utf-8") as f:
+    dataset_path = json.load(f)
+    if dataset_path["datasets_dir"] != get_datasets_path():
+        with open(path_to_ultralytics, "w", encoding="utf-8") as file:
+            dataset_path["datasets_dir"] = get_datasets_path()
+            json.dump(dataset_path, file, indent=0)
+
+    from ultralytics import YOLO
 
 
 def start_training(log_widget):
@@ -31,10 +62,14 @@ def params_designation(log_widget=""):
     Возвращает:
     - params (dict): Словарь с параметрами для обучения
     """
+
+    # Set up the logger
     if log_widget != "":
+        logger = logging.getLogger('ultralytics')
         log_redirector = LogRedirector(log_widget)
         sys.stderr = log_redirector  # Захватываем tqdm и ошибки
-        logging.basicConfig(stream=sys.stderr, level=logging.INFO, force=True)  # Перенаправляем logging
+        logger.handlers[0].stream = log_redirector
+        logging.basicConfig(stream=logger.handlers[0].stream, level=logging.INFO, force=True)  # Перенаправляем logging
 
     params = {
         "device": 0 if load_data("Тип графического устройства") == 0 else 'cpu',
@@ -53,7 +88,6 @@ def params_designation(log_widget=""):
     return params
 
 
-@retry(num_retries=1, exception_to_check=RuntimeError, sleep_time=1)
 def train_yolo_model(log_widget):
     """
     Обучает модель YOLO на пользовательском наборе данных и контроль за флагом запуска.
@@ -75,4 +109,5 @@ def train_yolo_model(log_widget):
         epochs=params["epochs"],
         data=params["data_yaml_path"],
     )
+
     print("Обучение завершено. Результаты:", results)
