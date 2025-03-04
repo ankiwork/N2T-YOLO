de09c02180a266b7c2641a3ee1d43cf076c3c85e
Svin777 <ar.nazarenko@mail.ru>
2025-03-04 15:59:48 +0300

    Added launch control

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flet import app
 
 from project.application.build import building_the_application
-from project.application.backend.cascade_control import check_file_settings
+from project.application.backend.cascade_control import check_file_settings, update_file_settings
 
-check_file_settings()
 
-app(target=building_the_application)
+if check_file_settings() == 0:
+    update_file_settings()
+    app(target=building_the_application)
+    update_file_settings()
--- project/application/backend/cascade_control.py
+++ project/application/backend/cascade_control.py
@@ -21,6 +21,24 @@ def check_file_settings():
         with open(settings_file, "w", encoding="utf-8") as f:
             json.dump(default_settings, f, indent=4)
 
+    with open(settings_file, "r", encoding="utf-8") as f:
+        settings = json.load(f)
+        return settings["launch_denial"]
+
+
+def update_file_settings():
+    """
+    Вспомогательная функция контроля флага
+
+    Возвращает:
+    None
+    """
+    with open(settings_file, "r", encoding="utf-8") as f:
+        settings = json.load(f)
+        settings["launch_denial"] = 1 - settings["launch_denial"]
+    with open(settings_file, "w", encoding="utf-8") as f:
+        json.dump(settings, f, indent=4)
+
 
 def reset_settings(controls, settings):
     """
